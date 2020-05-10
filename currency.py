970a6d01414ae3492ef76b61702eea3d77457766
63815af756f0357940c666d2ae1c801d <46129173@qq.com>
2020-05-10 19:41:51 +0800

    add pay method

--- currency.py
+++ currency.py
@@ -11,6 +11,7 @@ def getmoney(server, info, playername, moneytype):
         server.tell(info.player, 'Warning: ' + playername + ' haven''t had any ' + moneytype)
         return 0
 
+
 def getmoney_svr(server, playername, moneytype):
     resultstr = server.rcon_query('scoreboard players get ' + playername + ' ' + moneytype)
     if (re.fullmatch(r'\w+ has \d* .*', resultstr)):
@@ -19,6 +20,7 @@ def getmoney_svr(server, playername, moneytype):
     else:
         print('Warning: ' + playername + ' haven''t had any ' + moneytype)
         return 0
+
 
 def getmoney_pr(server, playername, moneytype):
     return server.rcon_query('scoreboard players get ' + playername + ' ' + moneytype)
@@ -36,9 +38,23 @@ def submoney(server, playername, moneytype, val):
     server.execute('scoreboard players remove ' + playername + ' ' + moneytype + ' ' + str(val))
 
 
+def trypaymoney(server, fromname, toname, moneytype, val):
+    maxmoney = 2147483647
+    srcval = getmoney_svr(server, fromname, moneytype)
+    if (srcval < val):
+        server.tell(fromname, 'You have not enough money!')
+        return
+    toval = getmoney_svr(server, toname, moneytype)
+    if (toval + val > maxmoney):
+        server.tell(fromname, toname + ' can not receive that much money!')
+        return
+    submoney(server, fromname, moneytype, val)
+    addmoney(server, toname, moneytype, val)
+
+
 def onServerInfo(server, info):
     info2 = copy.deepcopy(info)
-    info2.is_player=info.isPlayer
+    info2.is_player = info.isPlayer
     on_info(server, info2)
 
 
@@ -124,6 +140,17 @@ def on_info(server, info):
             playername = args[1]
             cointype = args[2]
             print(str(getmoney_pr(server, playername, cointype)))
+        if info.content.startswith("!!pay "):
+            args = info.content.split(" ")
+            if (len(args) != 4):
+                print('!!pay <player> <type> amount')
+                return
+            playername = args[1]
+            cointype = args[2]
+            coinamount = int(args[3])
+            if (coinamount <= 0):
+                server.tell(info.player, 'You must pay a positive number!')
+            trypaymoney(server, info.player, playername, cointype, coinamount)
 
 
 def on_load(server, old):
@@ -131,3 +158,4 @@ def on_load(server, old):
     server.add_help_message('!!take', 'Take money from someone')
     server.add_help_message('!!set', 'Set money for someone')
     server.add_help_message('!!get', 'Get money of someone')
+    server.add_help_message('!!pay', 'Pay money to someone')
